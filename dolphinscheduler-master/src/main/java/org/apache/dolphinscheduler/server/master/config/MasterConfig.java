a85304952d51ed758e90b00c8520ff5f0262024d
eye <eye.gu@aloudata.com>
2023-11-21 14:48:37 +0800

    id

--- dolphinscheduler-master/src/main/java/org/apache/dolphinscheduler/server/master/config/MasterConfig.java
+++ dolphinscheduler-master/src/main/java/org/apache/dolphinscheduler/server/master/config/MasterConfig.java
@@ -27,6 +27,7 @@ import org.apache.dolphinscheduler.server.master.runner.WorkflowExecuteRunnable;
 import org.apache.commons.lang3.StringUtils;
 
 import java.time.Duration;
+import java.util.UUID;
 
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
@@ -102,6 +103,8 @@ public class MasterConfig implements Validator {
     // ip:listenPort
     private String masterAddress;
 
+    private String id;
+
     // /nodes/master/ip:listenPort
     private String masterRegistryPath;
 
@@ -157,8 +160,10 @@ public class MasterConfig implements Validator {
             masterConfig.setMasterAddress(NetUtils.getAddr(masterConfig.getListenPort()));
         }
 
+        masterConfig.setId(masterAddress + "_" + UUID.randomUUID().toString().replaceAll("-", ""));
+
         masterConfig.setMasterRegistryPath(
-                RegistryNodeType.MASTER.getRegistryPath() + "/" + masterConfig.getMasterAddress());
+                RegistryNodeType.MASTER.getRegistryPath() + "/" + masterConfig.getId());
         printConfig();
     }
 
